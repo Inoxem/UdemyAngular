8ffe6b5137da95f69530fea93c657544e9a240fc
Christian <christian.farley@inoxem.ca>
2019-04-02 18:22:33 -0400

    CF0402: Premier push

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
+  serverCreated = false;
+
+  constructor() { 
+      setTimeout(() =>{
+        this.allowNewServer = true;
+      }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created. Name is:'+this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
